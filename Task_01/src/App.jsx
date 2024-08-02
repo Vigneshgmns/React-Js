cf756a5e0b603cdf1ce1639b91ca31a742760502
Vignesh G <148624660+Vigneshgmns@users.noreply.github.com>
2024-08-02 15:26:40 +0530

    create single element selection

--- Task_01/src/App.jsx
+++ Task_01/src/App.jsx
@@ -1,14 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [datas, setDatas] = useState(null)
+
+  const handelData = (id) => {
+    setDatas(id);
+  };
+
+  const data = [
+    {
+      id: 1,
+      qusetion: "What is the best way to learn React.js?",
+      answer: "React.js is a JavaScript library for building user interfaces"
+    }, {
+      id: 2,
+      qusetion: "What is the best way to learn React.js?",
+      answer: "React.js is a JavaScript library for building user interfaces"
+    }, {
+      id: 3,
+      qusetion: "What is the best way to learn React.js?",
+      answer: "React.js is a JavaScript library for building user interfaces"
+    }, {
+      id: 4,
+      qusetion: "What is the best way to learn React.js?",
+      answer: "React.js is a JavaScript library for building user interfaces"
+    }]
+
 
   return (
     <>
-    
+      <div>
+        {
+          data && data.length > 0 ?
+            <div>
+              {
+                data.map((item) => (
+                  <div>
+                    <div onClick={() => handelData(item.id)}>
+                      <h3>{item.qusetion}</h3>
+                      <span>+</span>
+                    </div>
+                    {
+                      datas === item.id ? <div>{item.answer}</div> : <div></div>
+                    }
+                  </div>
+                ))
+              }
+            </div>
+            :
+            <div>No data found</div>
+        }
+      </div>
+
     </>
   )
 }
